33e6193b268734fb7b3392c4338693d5400292dc
benchiz <trigubov.art@mail.ru>
2026-03-05 13:20:43 +0300

    delete empty string in config file (pseudo-beautify)

--- app/config.py
+++ app/config.py
@@ -9,7 +9,6 @@ class Settings(BaseSettings):
     VERSION: str = os.getenv("VERSION", "0.1.0")
     PORT: int = int(os.getenv("PORT", "8000"))
     SERVICE_NAME: str = "cryptocurrency"
-
     COINGECKO_API_URL: str = "https://api.coingecko.com/api/v3"
 
     class Config:
